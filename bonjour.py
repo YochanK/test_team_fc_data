64ce668b1dc0a11eb5f8be9de9a0a64ed0c17c32
Ricodeladata <emhak01@hotmail.fr>
2021-11-05 20:30:56 +0100

    ajout création dataframe

--- bonjour.py
+++ bonjour.py
@@ -1,5 +1,9 @@
+import pandas as pd
+
 print("Bonjour")
 print("c'est cool git")
 print("je suis sur la branch premier_branch")
 print("lalalalla")
 print('rayayayyyy')
+
+df = pd.DataFrame(data=[1, 2], columns=['A', 'B'])
