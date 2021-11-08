d35b8a18571a868549df4c7108b47a7b6e666dc5
Ricodeladata <emhak01@hotmail.fr>
2021-11-08 14:45:54 +0100

    j'ai m

--- bonjour.py
+++ bonjour.py
@@ -6,7 +6,9 @@ print("je suis sur la branch premier_branch")
 print("lalalalla")
 print('rayayayyyy')
 
-dictio1 = {'A':1, 'B':2}
-df=pd.DataFrame(data=dictio1)
+dictio1 = {'A':[1], 'B':[2]}
+df = pd.DataFrame(data=dictio1)
 
 print('Clem installed git ')
+x = 5+7
+print('je vais travailler '+str(x)+' heures')
